333693771a1fe8ab1fe05cfee32fb0f3aa65afcb
izzyn <isak.attila@gmail.com>
2024-09-17 13:17:41 +0200

    Worked on type parsing

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,31 +1,101 @@
-use core::time;
-use std::io::prelude::*;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream};
-use std::str::FromStr;
-use std::thread::{sleep, sleep_ms};
-use std::time::Duration;
+// src/bin/client.rs
+use std::error::Error;
+use std::io;
+use tokio::io::AsyncReadExt;
+use tokio::net::TcpStream;
 
-fn main() -> std::io::Result<()> { 
-    println!("Connecting to server...");
-    let mut stream : TcpStream; 
-    loop{
+enum Type {
+    String,
+    U32,
+    U64,
+    Usize,
+    Signal,
+    BeginArgs,
 
-        stream = match TcpStream::connect_timeout(&SocketAddr::new(IpAddr::from(Ipv4Addr::from_str("127.0.0.1").expect("Incorrect IP adress was given")), 34254), Duration::new(5, 5)) {
-            Err(err) => {
-                println!("Failed to connect");
-                println!("Retrying!");
-                continue;
-            },
-            Ok(i) => i,
-        };
-        break;
+}
+
+struct BuffData {
+    len : [u8; 4],
+    d_type : Type,
+
+
+}
+
+impl BuffData {
+    fn write_len(&mut self, len : u32){
+        let bytes = len.to_be_bytes();
+        self.len = bytes;
+
+    }
+    
+}
+
+
+#[tokio::main]
+async fn main() {
+    let server_addr = "127.0.0.1:1234";
+    let stream: TcpStream = TcpStream::connect(server_addr).await.unwrap();
+    println!("Connected to {}", stream.peer_addr().unwrap());
+    send_request(stream).await.unwrap();
+}
+
+async fn send_request(stream: TcpStream) -> Result<(), Box<dyn Error>> {
+
+    let mut buf = BuffData{
+        d_type: Type::String,
+        len: 9999999_u32.to_be_bytes(),
+    };
+    println!("{}", buf.len[0]);
+    
+    loop {
+        println!("Expecting input...");
+        let mut close = false;
+        let mut buffer = String::new();
+        let stdin = io::stdin(); // We get `Stdin` here.
+        stdin.read_line(&mut buffer)?;
+        match buffer.trim(){
+            "close" => close = true,
+            _ => {println!("Not real argument");},
+        }
+        loop {
+            stream.writable().await?;
+            match stream.try_write(b"Hello!") {
+                Ok(n) => {
+                    println!("Wrote {} bytes", n);
+                    break;
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(e) => {
+                    return Err(e.into());
+                }
+            }
+        }
+
+        let mut buf: [u8; 4096];
+        loop {
+            stream.readable().await?;
+            buf = [0; 4096];
+            match stream.try_read(&mut buf) {
+                Ok(n) => {
+                    let mut vec = Vec::with_capacity(n);
+                    buf.take(n as u64).read_to_end(&mut vec).await?;
+                    let s = String::from_utf8(buf.to_vec()).unwrap();
+                    println!("Got reply from host: {}", s);
+                    break;
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(e) => return Err(e.into()),
+            }
+        }
+        if close{
+            println!("Amongus");
+            break Ok(());
+        }
     }
 
-    println!("Established connection");
-    loop{
-        stream.write(&[1])?;
-        sleep_ms(1000);
-        stream.read(&mut [0; 128])?;
-    }
-    Ok(())
-} // the stream is closed here
+}
+
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,40 +1,84 @@
-use std::{
-    collections::hash_map::HashMap, 
-    io::{prelude::*, BufReader}, net::{SocketAddr, TcpListener, TcpStream}
+// src/bin/server.rs
+use std::{error::Error, i32, io};
+use tokio::{
+    io::{AsyncReadExt, Interest},
+    net::{TcpListener, TcpStream},
 };
+use core::net::SocketAddr;
 
 struct Client {
-    ualive : bool,
-    address : SocketAddr,
+    sendpkts : Vec<i32>,
+    sendbfr : [i32; 2048],
+    readbfr : [i32; 2048],
+    addr : SocketAddr,
+    
 }
-fn main() {
-    let mut clients : HashMap<SocketAddr, &Client> = HashMap::new();
-    let listener = TcpListener::bind("127.0.0.1:34254").unwrap();
 
-    for stream in listener.incoming() {
-        let stream = match stream {
-            Err(err) => {
-                println!("Recieved stream error {err:?}");
-                continue;
-            },
-            Ok(i) => i,
+
+#[tokio::main]
+async fn main() {
+    let bind_addr = "0.0.0.0:1234";
+    listen(bind_addr).await;
+}
+
+async fn listen(bind_addr: &str) {
+    let listener = TcpListener::bind(bind_addr).await.unwrap();
+
+    loop {
+        let (stream, _) = match listener.accept().await {
+            Ok(t) => t,
+            Err(_e) => continue,
         };
-        let _ = stream.set_nonblocking(true);
-
-        handle_connection(stream, &clients);
-        println!("Connection established!");
+        tokio::spawn(async move {
+            match handle_stream(stream).await {
+                Ok(t) => t,
+                Err(e) => println!("{:?}", e),
+            };
+        });
     }
 }
 
-fn handle_connection(mut stream : TcpStream, mut clients : &HashMap<SocketAddr, &Client>){
-    let addr = match stream.peer_addr(){
-        Ok(adress) => adress,
-        Err(err) => {
-            println!("Got error: {err:?} while trying to recover the peer adress");
-            return;
+async fn handle_stream(stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    println!("Connection from {}", stream.peer_addr().unwrap());
+    let mut reply_queue: Vec<Vec<u8>> = Vec::new();
+    let mut buf: [u8; 1024];
+    
+    loop {
+        let ready = stream
+            .ready(Interest::READABLE | Interest::WRITABLE)
+            .await?;
+        if ready.is_readable() {
+	    buf = [0; 1024];
+            match stream.try_read(&mut buf) {
+                Ok(n) => {
+                    println!("read {} bytes", n);
+                    let mut result_buffer: Vec<u8> = Vec::with_capacity(n);
+                    buf.take(n as u64).read_to_end(&mut result_buffer).await?;
+                    reply_queue.push(buf.to_vec());
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(e) => {
+                    return Err(e.into());
+                }
+            }
         }
-    };
-    
-    
-    println!("Got connection from {}", addr);
+
+        if ready.is_writable() {
+            if let Some(msg) =  reply_queue.pop() {
+                match stream.try_write(&msg) {
+                    Ok(n) => {
+                        println!("Wrote {} bytes", n);
+                    }
+                    Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                        continue;
+                    }
+                    Err(e) => {
+                        return Err(e.into());
+                    }
+                }
+            }
+        }
+    }
 }
