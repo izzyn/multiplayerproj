2c48c49e174380f017bdb739220f30b6f9a1fbeb
Spizzy1 <isak.attila@gmail.com>
2024-10-03 00:33:26 +0200

    Major code refactoring + working on string parsing

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,7 +1,9 @@
 //shared/src/lib.rs
 pub mod data {
 use core::f32;
+use std::str::from_utf8;
 use std::{error::Error, fmt, num::NonZeroUsize, str::FromStr, usize};
+use std::fs;
 
 #[derive(Debug)]
 pub struct DataParseError {
@@ -142,15 +144,24 @@ pub fn encode_string(data : String) -> Result<Vec<u8>, DataParseError> {
     let mut lencopy = length;
     let mut n = 1;
     while lencopy != 0 {
+        if n != 1 {
+            n *= 2;
+        }
         lencopy >>= 8 * n;
-        n *= 2;
     }
-    match n {}
-    println!("{}", n);
     let mut returndata : Vec<u8> = Vec::new();
-    returndata[1..].copy_from_slice(&data.into_bytes());
-    returndata[0] = DataIDs::STRING as u8;
-    return returndata;
+    returndata.push(DataIDs::STRING as u8);
+    match n {
+        1 => returndata.extend_from_slice(&encode_u8(length.try_into().unwrap())),
+        2 => returndata.extend_from_slice(&encode_u16(length.try_into().unwrap())),
+        4 => returndata.extend_from_slice(&encode_u32(length.try_into().unwrap())),
+        8 => returndata.extend_from_slice(&encode_u64(length.try_into().unwrap())),
+        _ => return Err(DataParseError {message : "Your string is ridiculously long, or the size calculation is badly written. Equally likely".to_string()}),
+
+    }
+    println!("{}", n);
+    returndata.extend_from_slice(&data.into_bytes());
+    return Ok(returndata);
 }
 
 
@@ -178,75 +189,43 @@ pub fn parse(bytes : &[u8]) -> Result<ParsedTree, DataParseError> {
         }
         let datatypeerr = "Error when parsing ".to_string();
         let data = match DataIDs::try_from(bytes[typeidx]).ok().ok_or(DataParseError{message : "Attempted to parse invalid datatype".to_string()})?{
-            DataIDs::U8 => {
-                let parsed = ParsedData::U8(u8::from_be_bytes(*bytes[idx..].first_chunk::<1>().ok_or(
-                        DataParseError{message : format!("{datatypeerr} U8")})?));
-                idx += 1;
-                parsed
+            DataIDs::U8 | DataIDs::U16 | DataIDs::U32 | DataIDs::U64 | DataIDs::I8 | DataIDs::I16 | DataIDs::I32 | DataIDs::I64 | DataIDs::F32 | DataIDs::F64 | DataIDs::CHAR => {
+                let result : (ParsedData, usize)= parse_basic(&bytes[typeidx..])?; 
+                idx += result.1;
+                result.0
             },
-            DataIDs::U16 => {
-                let parsed = ParsedData::U16(u16::from_be_bytes(*bytes[idx..].first_chunk::<2>().ok_or(
-                        DataParseError{message : format!("{datatypeerr} U16")})?));
-                idx += 2;
-                parsed
-            },
-            DataIDs::U32 => {
-                let parsed = ParsedData::U32(u32::from_be_bytes(*bytes[idx..].first_chunk::<4>().ok_or(
-                        DataParseError{message : format!("{datatypeerr} U32")})?));
-                idx += 4;
-                parsed
-            },
-            DataIDs::U64 => {
-                let parsed = ParsedData::U64(u64::from_be_bytes(*bytes[idx..].first_chunk::<8>().ok_or(
-                        DataParseError{message : format!("{datatypeerr} U64")})?));
-                idx += 8;
-                parsed
-            },
-            DataIDs::I8 => {
-                let parsed = ParsedData::I8(i8::from_be_bytes(*bytes[idx..].first_chunk::<1>().ok_or(
-                        DataParseError{message : format!("{datatypeerr} I8")})?));
-                idx += 1;
-                parsed
-            },
-            DataIDs::I16 => {
-                let parsed = ParsedData::I16(i16::from_be_bytes(*bytes[idx..].first_chunk::<2>().ok_or(
-                        DataParseError{message : format!("{datatypeerr} I16")})?));
-                idx += 2;
-                parsed
-            },
-            DataIDs::I32 => {
-                let parsed = ParsedData::I32(i32::from_be_bytes(*bytes[idx..].first_chunk::<4>().ok_or(
-                        DataParseError{message : format!("{datatypeerr} I32")})?));
-                idx += 4;
-                parsed
-            },
-            DataIDs::I64 => {
-                let parsed = ParsedData::I64(i64::from_be_bytes(*bytes[idx..].first_chunk::<8>().ok_or(
-                        DataParseError{message : format!("{datatypeerr} I64")})?));
-                idx += 8;
-                parsed
-            },
-            DataIDs::CHAR => {
-                let parsed = ParsedData::CHAR(char::from_u32(u32::from_be_bytes(*bytes[idx..].first_chunk::<4>().ok_or(
-                        DataParseError{message : format!("{datatypeerr} CHAR")})?)).unwrap());
-                idx += 4;
-                parsed
-            },
-            DataIDs::F32 => {
-                let parsed = ParsedData::F32(f32::from_be_bytes(*bytes[idx..].first_chunk::<4>().ok_or(
-                        DataParseError{message : format!("{datatypeerr} F32")})?));
-                idx += 4;
-                parsed
-            },
-            DataIDs::F64 => {
-                let parsed = ParsedData::F64(f64::from_be_bytes(*bytes[idx..].first_chunk::<8>().ok_or(
-                        DataParseError{message : format!("{datatypeerr} F64")})?));
-                idx += 8;
-                parsed
+            DataIDs::STRING => {
+                let result = parse_basic(&bytes[typeidx..])?;
+                idx += result.1;
+                let length = match result.0 {
+                    ParsedData::U8(t) => {
+                        t as usize
+                    },
+                    ParsedData::U16(t) => {
+                        t as usize
+                    },
+                    ParsedData::U32(t) => {
+                        t as usize
+                    },
+                    ParsedData::U64(t) => {
+                        t as usize
+                    },
+                    _ => {return Err(DataParseError {message : "String length was of an invalid type (did you try to use a signed integer?)".to_string()})}
+                };
+                if bytes.len() >= idx {
+                    let string = match from_utf8(&bytes[idx..length]) {
+                            Ok(t) => t.to_string(),
+                            Err(e) => return Err(DataParseError {message : "String wasn't a valid utf8 string".to_string()})
+                        };
+                    ParsedData::STRING(string)
+                }
+                else{
+                    return Err(DataParseError { message: "Client lied about string length".to_string() })
+                }
             },
             DataIDs::ENDPKG => {
                 return Ok(tree)
-            }
+            },
             _ => return Err(DataParseError {message : String::from("Attempted to parse an invalid datatype")})
         };
 
@@ -257,7 +236,7 @@ pub fn parse(bytes : &[u8]) -> Result<ParsedTree, DataParseError> {
         let node = ParsedNode {
             parent : currentparent,
             next : None,
-            data : data,
+            data,
             prev : previousid,
         };
         previousid = Some(currentid);
@@ -267,16 +246,83 @@ pub fn parse(bytes : &[u8]) -> Result<ParsedTree, DataParseError> {
     return Err(DataParseError {message : String::from("Recieved data without a proper end signal.")});
 }
 
-impl ParsedData{
-    fn parse_node(bytes : &[u8], tree : &mut ParsedTree) -> Result<ParsedNode, DataParseError> {
-            return Err(DataParseError { message : String::from("Couldn't parse node'")})
-
+fn parse_basic(bytes : &[u8]) -> Result<(ParsedData, usize), DataParseError> {
+    let mut idx = 0;
+    let datatypeerr = "Error when parsing ".to_string();
+    let data = match DataIDs::try_from(bytes[0]).ok().ok_or(DataParseError{message : "Attempted to parse invalid datatype".to_string()})?{
+        DataIDs::U8 => {
+            let parsed = ParsedData::U8(u8::from_be_bytes(*bytes[1..].first_chunk::<1>().ok_or(
+                    DataParseError{message : format!("{datatypeerr} U8")})?));
+            idx += 1;
+            return Ok((parsed,idx));
+        },
+        DataIDs::U16 => {
+            let parsed = ParsedData::U16(u16::from_be_bytes(*bytes[1..].first_chunk::<2>().ok_or(
+                    DataParseError{message : format!("{datatypeerr} U16")})?));
+            idx += 2;
+            return Ok((parsed,idx));
+        },
+        DataIDs::U32 => {
+            let parsed = ParsedData::U32(u32::from_be_bytes(*bytes[1..].first_chunk::<4>().ok_or(
+                    DataParseError{message : format!("{datatypeerr} U32")})?));
+            idx += 4;
+            return Ok((parsed,idx));
+        },
+        DataIDs::U64 => {
+            let parsed = ParsedData::U64(u64::from_be_bytes(*bytes[1..].first_chunk::<8>().ok_or(
+                    DataParseError{message : format!("{datatypeerr} U64")})?));
+            idx += 8;
+            return Ok((parsed,idx));
+        },
+        DataIDs::I8 => {
+            let parsed = ParsedData::I8(i8::from_be_bytes(*bytes[1..].first_chunk::<1>().ok_or(
+                    DataParseError{message : format!("{datatypeerr} I8")})?));
+            idx += 1;
+            return Ok((parsed,idx));
+        },
+        DataIDs::I16 => {
+            let parsed = ParsedData::I16(i16::from_be_bytes(*bytes[1..].first_chunk::<2>().ok_or(
+                    DataParseError{message : format!("{datatypeerr} I16")})?));
+            idx += 2;
+            return Ok((parsed,idx));
+        },
+        DataIDs::I32 => {
+            let parsed = ParsedData::I32(i32::from_be_bytes(*bytes[1..].first_chunk::<4>().ok_or(
+                    DataParseError{message : format!("{datatypeerr} I32")})?));
+            idx += 4;
+            return Ok((parsed,idx));
+        },
+        DataIDs::I64 => {
+            let parsed = ParsedData::I64(i64::from_be_bytes(*bytes[1..].first_chunk::<8>().ok_or(
+                    DataParseError{message : format!("{datatypeerr} I64")})?));
+            idx += 8;
+            return Ok((parsed,idx));
+        },
+        DataIDs::CHAR => {
+            let parsed = ParsedData::CHAR(char::from_u32(u32::from_be_bytes(*bytes[1..].first_chunk::<4>().ok_or(
+                    DataParseError{message : format!("{datatypeerr} CHAR")})?)).unwrap());
+            idx += 4;
+            return Ok((parsed,idx));
+        },
+        DataIDs::F32 => {
+            let parsed = ParsedData::F32(f32::from_be_bytes(*bytes[1..].first_chunk::<4>().ok_or(
+                    DataParseError{message : format!("{datatypeerr} F32")})?));
+            idx += 4;
+            return Ok((parsed,idx));
+        },
+        DataIDs::F64 => {
+            let parsed = ParsedData::F64(f64::from_be_bytes(*bytes[1..].first_chunk::<8>().ok_or(
+                    DataParseError{message : format!("{datatypeerr} F64")})?));
+            idx += 8;
+            return Ok((parsed,idx));
+        },
+        _ => {
+            return Err(DataParseError{message : "Tried to parse non-basic datatype as a basic datatype".to_string()})
         }
-    }
-    
 
+    };
 }
-
+}
 macro_rules! test_encoding {
     ($type : ty, $enumvalue : ident) => {
         ::paste::paste! {
@@ -294,6 +340,7 @@ macro_rules! test_encoding {
                         buff[i] = test_bytes[i];
                     }
                     buff[SIZE - 1] = DataIDs::ENDPKG as u8;
+                    println!("End Signal: {}", buff[SIZE-1]);
                     let tree = match parse(&buff) {
                         Ok(t) => t,
                         Err(e) => panic!("{:?}", e),
@@ -312,7 +359,8 @@ macro_rules! test_encoding {
 }
 #[cfg(test)]
 mod tests {
-    use std::{i32, u8};
+    use core::panic;
+    use std::{fs, i32, u8};
 
     use super::*;
     use data::*;
@@ -350,6 +398,7 @@ mod tests {
 
     #[allow(non_snake_case, non_upper_case_globals)]
     const TESTS_char : [char; 5] = ['Ä','\\', '\n', 'å', 'A'];
+
     //Test functions for primitive types
     test_encoding!(u8, U8);
     test_encoding!(u16, U16);
@@ -362,29 +411,33 @@ mod tests {
     test_encoding!(f32, F32);
     test_encoding!(f64, F64);
     test_encoding!(char, CHAR);
-    //#[test]
-    //fn test_encoding_string(){
-    //    println!("{}", SIZE);
-    //    let mut buff : [u8 ; SIZE] = [0; SIZE];
-    //    
-    //    for j in 0..[<TESTS_ $type>].len() {
-    //        println!("{}", [<TESTS_ $type>].len());
-    //        let test_bytes = [<encode_ $type>]([<TESTS_ $type>][j]);
-    //        println!("{}", test_bytes.len());
-    //        for i in 0..test_bytes.len() {
-    //            buff[i] = test_bytes[i];
-    //        }
-    //        buff[SIZE - 1] = DataIDs::ENDPKG as u8;
-    //        let tree = match parse(&buff) {
-    //            Ok(t) => t,
-    //            Err(e) => panic!("{:?}", e),
-    //        };
-    //        
-    //        let data = tree.nodes[0].data.clone();
-    //        match data {
-    //            ParsedData::$enumvalue(contents) => assert_eq!([<TESTS_ $type>][j], contents),
-    //            _ => panic!("Incorrect datatype parsed"),
-    //        }
-    //    }
-    //}
+    #[test]
+    fn test_encoding_string(){
+        let  TESTS_String : [String; 5] = ["Räkmacka".to_string(),"I LOVE 'Escaping' \"STRINGS\"".to_string(), "I ^^, *** Sp3<!4L <h4r4<t3r2".to_string(), "This is straight up just a normal string".to_string(), fs::read_to_string("./testcase.txt").expect("I can't see your extremely funny text file test case :(")];
+        const SIZE : usize = 4096;
+        println!("{}", SIZE);
+        let mut buff : [u8 ; SIZE] = [0; SIZE];
+        
+        for j in 0..TESTS_String.len() {
+            let test_bytes = match encode_string(TESTS_String[j].clone()) {
+                Err(e) => panic!(),
+                Ok(t) => t
+            };
+            println!("{}", test_bytes.len());
+            for i in 0..test_bytes.len() {
+                buff[i] = test_bytes[i];
+            }
+            buff[SIZE - 1] = DataIDs::ENDPKG as u8;
+            let tree = match parse(&buff) {
+                Ok(t) => t,
+                Err(e) => panic!("{:?}", e),
+            };
+            
+            let data = tree.nodes[0].data.clone();
+            match data {
+                ParsedData::STRING(contents) => assert_eq!(TESTS_String[j], contents),
+                _ => panic!("Incorrect datatype parsed"),
+            }
+        }
+    }
 }
